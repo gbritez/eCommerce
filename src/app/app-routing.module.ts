7750ef9b28b32e443b6367c06efff163055ba3ad
Gonzalo Britez <gonzalo_britez@hotmail.com>
2019-09-20 15:07:27 -0300

    Added components and folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './Components/category/category.component';
+import { ProductDetailComponent } from './Components/product-detail/product-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'category', component: CategoryComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './Components/top-bar/top-bar.component';
+import { BottomBarComponent } from './Components/bottom-bar/bottom-bar.component';
+import { ProductCardComponent } from './Components/product-card/product-card.component';
+import { CategoryComponent } from './Components/category/category.component';
+import { ProductComponent } from './Components/product/product.component';
+import { ProductDetailComponent } from './Components/product-detail/product-detail.component';
+import { BoxComponent } from './Components/box/box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomBarComponent,
+    ProductCardComponent,
+    TopBarComponent,
+    CategoryComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
